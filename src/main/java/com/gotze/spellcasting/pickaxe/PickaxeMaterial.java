72a9e0e5a095f444668afff9bca0fd56562dc67a
Gotze <thorholby@gmail.com>
2025-09-15 00:39:02 +0200

    Add PickaxeMaterial enum to define pickaxe types and durability

---
+++ src/main/java/com/gotze/spellcasting/pickaxe/PickaxeMaterial.java
@@ -0,0 +1,33 @@
+package com.gotze.spellcasting.pickaxe;
+
+import org.bukkit.Material;
+
+public enum PickaxeMaterial {
+    WOOD(Material.WOODEN_PICKAXE, 256),
+    STONE(Material.STONE_PICKAXE, 512),
+    IRON(Material.IRON_PICKAXE, 1024),
+    GOLD(Material.GOLDEN_PICKAXE, 2048),
+    DIAMOND(Material.DIAMOND_PICKAXE, 4096),
+    NETHERITE(Material.NETHERITE_PICKAXE, 8192);
+
+    private final Material material;
+    private final int maxDurability;
+
+    PickaxeMaterial(Material material, int maxDurability) {
+        this.material = material;
+        this.maxDurability = maxDurability;
+    }
+
+    public Material getType() {
+        return material;
+    }
+
+    public int getMaxDurability() {
+        return maxDurability;
+    }
+
+    @Override
+    public String toString() {
+        return name().charAt(0) + name().substring(1).toLowerCase();
+    }
+}
