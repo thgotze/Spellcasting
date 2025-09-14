eb60509823ed7fd0c704ecf9b784e18ab54a134a
Gotze <thorholby@gmail.com>
2025-09-15 00:41:02 +0200

    Refactor PlayerPickaxeManager to enhance block break handling and command usage

--- src/main/java/com/gotze/spellcasting/pickaxe/PlayerPickaxeManager.java
+++ src/main/java/com/gotze/spellcasting/pickaxe/PlayerPickaxeManager.java
@@ -2,21 +2,58 @@ package com.gotze.spellcasting.pickaxe;
 
 import com.gotze.spellcasting.pickaxe.ability.Ability;
 import com.gotze.spellcasting.pickaxe.enchantment.Enchantment;
+import com.gotze.spellcasting.util.SoundUtils;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.datacomponent.DataComponentTypes;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+import java.util.Set;
 
 public class PlayerPickaxeManager implements Listener, BasicCommand {
+
+    @EventHandler
+    public void onBlockBreakActivateEnchants(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        if (!PlayerPickaxeService.isPlayerHoldingOwnPickaxe(player)) return;
+        ItemStack heldItem = player.getInventory().getItemInMainHand();
+
+        PickaxeData pickaxeData = PlayerPickaxeService.getPickaxeData(player);
+
+        int maxDamage = heldItem.getData(DataComponentTypes.MAX_DAMAGE);
+        int damage = heldItem.getData(DataComponentTypes.DAMAGE);
+
+        if (damage + 1 == maxDamage) {
+            event.setCancelled(true);
+            player.sendMessage("Warning! Pickaxe has low durability!");
+            player.addPotionEffect(new PotionEffect(PotionEffectType.MINING_FATIGUE, 100, 2));
+            player.playSound(player, Sound.ENTITY_VILLAGER_NO, SoundCategory.MASTER, 1.0f, 1.0f, 404);
+            SoundUtils.playErrorSound(player);
+            return;
+        }
+
+        Set<Enchantment> enchantments = pickaxeData.getEnchantments();
+        for (Enchantment enchantment : enchantments) {
+            enchantment.activate(player, event, pickaxeData);
+        }
+
+        pickaxeData.addBlocksBroken(1);
+        pickaxeData.setDamage(damage + 1);
+
+        heldItem.lore(PlayerPickaxeService.getPickaxeLore(pickaxeData));
+    }
 
     @EventHandler
     public void onHandSwapActivateAbilities(PlayerSwapHandItemsEvent event) {
@@ -31,52 +68,30 @@ public class PlayerPickaxeManager implements Listener, BasicCommand {
         }
     }
 
-    @EventHandler
-    public void onBlockBreakActivateEnchants(BlockBreakEvent event) {
-        Player player = event.getPlayer();
-        if (!PlayerPickaxeService.isPlayerHoldingOwnPickaxe(player)) return;
-        ItemStack heldItem = player.getInventory().getItemInMainHand();
-
-        PickaxeData pickaxeData = PlayerPickaxeService.getPickaxeData(player);
-        pickaxeData.addBlocksBroken(1);
-//        pickaxeData.addDamage(1); TODO: make damage read the actual itemstacks damage value
-
-        for (Enchantment enchantment : pickaxeData.getEnchantments()) {
-            enchantment.activate(player, event, pickaxeData);
-        }
-
-        List<Component> lore = PlayerPickaxeService.getPickaxeLore(pickaxeData);
-        heldItem.lore(lore);
-    }
-
     @Override
     public void execute(@NotNull CommandSourceStack commandSourceStack, @NotNull String[] args) {
         if (!(commandSourceStack.getSender() instanceof Player player)) return;
 
         if (args.length == 0) {
-            player.sendMessage(Component.text("Usage: /pickaxe <give|reset>")
-                    .color(NamedTextColor.RED));
+            player.sendMessage(Component.text("Usage: /pickaxe <get|reset>").color(NamedTextColor.RED));
             return;
         }
 
-        if (args[0].equalsIgnoreCase("give")) {
+        if (args[0].equalsIgnoreCase("get")) {
             if (PlayerPickaxeService.getPickaxeData(player) == null) {
                 PlayerPickaxeService.createPickaxeData(player);
             }
             player.getInventory().addItem(PlayerPickaxeService.getPickaxe(player));
-            player.sendMessage(Component.text("You received your pickaxe!")
-                    .color(NamedTextColor.GREEN));
+            player.sendMessage(Component.text("You received your pickaxe!").color(NamedTextColor.GREEN));
             return;
         }
 
         if (args[0].equalsIgnoreCase("reset")) {
             PlayerPickaxeService.createPickaxeData(player);
-            player.sendMessage(Component.text("Pickaxe data reset!")
-                    .color(NamedTextColor.GREEN));
+            player.sendMessage(Component.text("Pickaxe data reset!").color(NamedTextColor.GREEN));
             return;
         }
 
-        player.sendMessage(Component.text("Unknown subcommand. Usage: /pickaxe <give|reset>")
-                .color(NamedTextColor.RED));
+        player.sendMessage(Component.text("Unknown subcommand. Usage: /pickaxe <get|reset>").color(NamedTextColor.RED));
     }
 }
