9813ab643a2e3d5afaef4aafd041d56c300821c1
Gotze <thorholby@gmail.com>
2025-09-28 00:48:12 +0200

    Add Loot class for item drop management with chance and amount control

--- src/main/java/com/gotze/spellcasting/feature/pickaxe/ability/HammerAbility.java
+++ src/main/java/com/gotze/spellcasting/feature/pickaxe/ability/HammerAbility.java
@@ -3,6 +3,7 @@ package com.gotze.spellcasting.feature.pickaxe.ability;
 import com.gotze.spellcasting.Spellcasting;
 import com.gotze.spellcasting.feature.pickaxe.PickaxeData;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class HammerAbility extends Ability {
@@ -26,6 +27,6 @@ public class HammerAbility extends Ability {
                 }
 
             }
-        }.runTaskTimer(Spellcasting.INSTANCE, 0, 1);
+        }.runTaskTimer(JavaPlugin.getPlugin(Spellcasting.class), 0, 1);
     }
 }
