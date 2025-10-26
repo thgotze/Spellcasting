1459cc51fcd19aaff189cf0dbf4e911f26e462f4
Gotze <thorholby@gmail.com>
2025-10-26 22:55:02 +0100

    Better JVM args for server in build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,50 +28,41 @@ tasks {
         minecraftVersion("1.21.9")
         systemProperty("Paper.IgnoreJavaVersion", "true")
         systemProperty("LetMeReload", "true")
-        jvmArgs("-Xmx4G", "-Xms4G")
+        jvmArgs("-Xmx4G", "-Xms4G",
+            "-XX:+AlwaysPreTouch",
+            "-XX:+DisableExplicitGC",
+            "-XX:+ParallelRefProcEnabled",
+            "-XX:+PerfDisableSharedMem",
+            "-XX:+UnlockExperimentalVMOptions",
+            "-XX:+UseG1GC",
+            "-XX:G1HeapRegionSize=8M",
+            "-XX:G1HeapWastePercent=5",
+            "-XX:G1MaxNewSizePercent=40",
+            "-XX:G1MixedGCCountTarget=4",
+            "-XX:G1MixedGCLiveThresholdPercent=90",
+            "-XX:G1NewSizePercent=30",
+            "-XX:G1RSetUpdatingPauseTimePercent=5",
+            "-XX:G1ReservePercent=20",
+            "-XX:InitiatingHeapOccupancyPercent=15",
+            "-XX:MaxGCPauseMillis=200",
+            "-XX:MaxTenuringThreshold=1",
+            "-XX:SurvivorRatio=32")
 
-//        jvmArgs("-Xmx4G", "-Xms4G",
 //            "-XX:+AllowEnhancedClassRedefinition",
-//
+
 //            "-XX:ReservedCodeCacheSize=1G",
 //            "-XX:NonProfiledCodeHeapSize=256M",
 //            "-XX:ProfiledCodeHeapSize=256M"
-//        )
 
-//        jvmArgs(
-//            "-Xms4G",
-//            "-Xmx4G",
-//
-//            "-XX:+AlwaysPreTouch",
-//            "-XX:+DisableExplicitGC",
-//            "-XX:+ParallelRefProcEnabled",
-//            "-XX:+PerfDisableSharedMem",
-//            "-XX:+UnlockExperimentalVMOptions",
-//            "-XX:+UseG1GC",
-//            "-XX:G1HeapRegionSize=16M",
-//            "-XX:G1HeapWastePercent=5",
-//            "-XX:G1MaxNewSizePercent=40",
-//            "-XX:G1MixedGCCountTarget=4",
-//            "-XX:G1MixedGCLiveThresholdPercent=90",
-//            "-XX:G1NewSizePercent=30",
-//            "-XX:G1RSetUpdatingPauseTimePercent=5",
-//            "-XX:G1ReservePercent=20",
-//            "-XX:InitiatingHeapOccupancyPercent=15",
-//            "-XX:MaxGCPauseMillis=200",
-//            "-XX:MaxTenuringThreshold=1",
-//            "-XX:SurvivorRatio=32",
-//
 //            "-XX:ReservedCodeCacheSize=2G",
 //            "-XX:NonProfiledCodeHeapSize=1G",
 //            "-XX:ProfiledCodeHeapSize=1000M",
-//
+
 //            "-XX:+UseCodeCacheFlushing",
 //            "-XX:+SegmentedCodeCache",
-//
 //            "-XX:+UseStringDeduplication",
 //            "-XX:+UseFastUnorderedTimeStamps",
 //            "-XX:+OptimizeStringConcat",
-//
 //            "-Dusing.aikars.flags=https://mcflags.emc.gs",
 //            "-Daikars.new.flags=true"
 //        )
