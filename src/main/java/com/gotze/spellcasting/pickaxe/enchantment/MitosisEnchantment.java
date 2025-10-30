1c17508154d869f9eeaccfb13cdf91c5748440f3
Gotze <thorholby@gmail.com>
2025-10-30 23:50:28 +0100

    Revamp MitosisEnchantment to get ore blocks on the same plane as the mined block

--- src/main/java/com/gotze/spellcasting/pickaxe/enchantment/MitosisEnchantment.java
+++ src/main/java/com/gotze/spellcasting/pickaxe/enchantment/MitosisEnchantment.java
@@ -2,20 +2,30 @@ package com.gotze.spellcasting.pickaxe.enchantment;
 
 import com.gotze.spellcasting.pickaxe.PickaxeData;
 import com.gotze.spellcasting.pickaxe.capability.BlockBreakListener;
+import com.gotze.spellcasting.pickaxe.capability.BlockDamageListener;
 import com.gotze.spellcasting.util.block.BlockCategories;
 import com.gotze.spellcasting.util.block.BlockUtils;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockDamageEvent;
 
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static net.kyori.adventure.text.Component.text;
 
-public class MitosisEnchantment extends Enchantment implements BlockBreakListener {
+public class MitosisEnchantment extends Enchantment implements BlockBreakListener, BlockDamageListener {
+
+    private BlockFace blockFace;
 
     public MitosisEnchantment() {
         super(EnchantmentType.MITOSIS);
+    }
+
+    @Override
+    public void onBlockDamage(Player player, BlockDamageEvent event, PickaxeData pickaxeData) {
+        this.blockFace = event.getBlockFace();
     }
 
     @Override
@@ -23,13 +33,19 @@ public class MitosisEnchantment extends Enchantment implements BlockBreakListene
         if (!isNaturalBreak) return;
         if (!BlockCategories.ORE_BLOCKS.containsKey(block.getType())) return;
         // 5% activation chance
-        if (ThreadLocalRandom.current().nextDouble() > 0.05) return;
-        player.sendActionBar(getEnchantmentType().getFormattedName().append(text(" activated")));
+//        if (ThreadLocalRandom.current().nextDouble() > 0.05) return;
 
-        List<Block> candidateBlocks = BlockUtils.getBlocksInSpherePattern(block, 3, 3, 3);
+        List<Block> candidateBlocks = switch (blockFace) {
+            case NORTH, SOUTH -> BlockUtils.getBlocksInSquarePattern(block, 3, 3, 1);
+            case EAST, WEST -> BlockUtils.getBlocksInSquarePattern(block, 1, 3, 3);
+            case UP, DOWN -> BlockUtils.getBlocksInSquarePattern(block, 3, 1, 3);
+            default -> null;
+        };
+        if (candidateBlocks == null) return;
         candidateBlocks.removeIf(candidate -> !BlockCategories.FILLER_BLOCKS.contains(candidate.getType()));
-
         if (candidateBlocks.isEmpty()) return;
+
+        player.sendActionBar(getEnchantmentType().getFormattedName().append(text(" activated")));
 
         Block chosenBlock1 = candidateBlocks.remove(ThreadLocalRandom.current().nextInt(candidateBlocks.size()));
         chosenBlock1.setType(block.getType());
