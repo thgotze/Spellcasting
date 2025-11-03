038d18e18bf7c719b593e303fa2590cc91b01311
Gotze <thorholby@gmail.com>
2025-11-03 02:22:07 +0100

    Refactor main class for clarity

--- src/main/java/com/gotze/spellcasting/Spellcasting.java
+++ src/main/java/com/gotze/spellcasting/Spellcasting.java
@@ -1,11 +1,11 @@
 package com.gotze.spellcasting;
 
+import com.gotze.spellcasting.data.PlayerProfileManager;
 import com.gotze.spellcasting.machine.MachineManager;
 import com.gotze.spellcasting.mines.MineManager;
 import com.gotze.spellcasting.pickaxe.PlayerPickaxeManager;
 import com.gotze.spellcasting.util.LifecycleManager;
 import com.gotze.spellcasting.util.menu.MenuListener;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -25,37 +25,43 @@ public class Spellcasting extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
-
-        // Simple listeners (no lifecycle)
-        register(new MenuListener());
-        register(new LootPotManager());
-        register(new ResourcePackManager());
-
-        // Lifecycle managers
-        register(new MachineManager());
-        register(new MineManager());
-
-        // Special cases with commands
-        PlayerPickaxeManager pickaxeManager = new PlayerPickaxeManager();
-        register(pickaxeManager);
-        registerCommand("pickaxe", pickaxeManager);
-
-        // Non-listener, non-lifecycle
-        RecipeRegistry.registerRecipes();
-
-        lifecycleManagers.forEach(LifecycleManager::start);
-    }
-
-    private void register(Object object) {
         PluginManager pluginManager = getServer().getPluginManager();
 
-        if (object instanceof Listener listener) {
-            pluginManager.registerEvents(listener, this);
-        }
+        // Player profile manager
+        PlayerProfileManager playerProfileManager = new PlayerProfileManager();
+        pluginManager.registerEvents(playerProfileManager, this);
 
-        if (object instanceof LifecycleManager lifecycleManager) {
-            lifecycleManagers.add(lifecycleManager);
-        }
+        // Menu listener
+        MenuListener menuListener = new MenuListener();
+        pluginManager.registerEvents(menuListener, this);
+
+        // Loot pot manager
+        LootPotManager lootPotManager = new LootPotManager();
+        pluginManager.registerEvents(lootPotManager, this);
+
+        // Resource pack manager
+        ResourcePackManager resourcePackManager = new ResourcePackManager();
+        pluginManager.registerEvents(resourcePackManager, this);
+
+        // Machine manager
+        MachineManager machineManager = new MachineManager();
+        pluginManager.registerEvents(resourcePackManager, this);
+        lifecycleManagers.add(machineManager);
+
+        // Mine manager
+        MineManager mineManager = new MineManager();
+        lifecycleManagers.add(mineManager);
+
+        // Player pickaxe manager
+        PlayerPickaxeManager pickaxeManager = new PlayerPickaxeManager();
+        pluginManager.registerEvents(pickaxeManager, this);
+        registerCommand("pickaxe", pickaxeManager);
+
+        // Recipe registry
+        RecipeRegistry.registerRecipes();
+
+        // Life cycle managers
+        lifecycleManagers.forEach(LifecycleManager::start);
     }
 
     @Override
