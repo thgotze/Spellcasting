666f7b57951258d9f84949761285033bad8fc4cd
Gotze <thorholby@gmail.com>
2025-11-07 02:03:54 +0100

    Re-enabled ResourcePackManager

--- src/main/java/com/gotze/spellcasting/ResourcePackManager.java
+++ src/main/java/com/gotze/spellcasting/ResourcePackManager.java
@@ -1,5 +1,6 @@
 package com.gotze.spellcasting;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,8 +10,9 @@ public class ResourcePackManager implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         // Set resource pack TODO: currently using clientside resourcepack instead of serverside
-//        String resourcePackUrl = "https://download.mc-packs.net/pack/7e609ad3d7d2abf0668002fa98c9d198ab544fe7.zip";
-//        player.sendMessage("Setting resource pack from file: " + resourcePackUrl);
-//        player.setResourcePack(resourcePackUrl);
+        Player player = event.getPlayer();
+        String resourcePackUrl = "https://download.mc-packs.net/pack/1596d8f15753ebb16f6870979ad427df8deb880a.zip";
+        player.sendMessage("Setting resource pack from file: " + resourcePackUrl);
+        player.setResourcePack(resourcePackUrl);
     }
 }
