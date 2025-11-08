1bdbd85debc9fd1ac90dc6de07e8b81754ebd5b0
Gotze <thorholby@gmail.com>
2025-11-08 01:40:08 +0100

    Add upgraded ore variants to OreExp enum

--- src/main/java/com/gotze/spellcasting/pickaxe/enchantment/InquisitiveEnchantment.java
+++ src/main/java/com/gotze/spellcasting/pickaxe/enchantment/InquisitiveEnchantment.java
@@ -41,12 +41,30 @@ public class InquisitiveEnchantment extends Enchantment implements BlockBreakLis
 
     public enum OreExp {
         NETHER_GOLD_ORE(Material.NETHER_GOLD_ORE, 0, 1),
-        COAL_ORE(Material.COAL_ORE,0, 2),
-        REDSTONE_ORE(Material.REDSTONE_ORE,1, 5),
-        LAPIS_ORE(Material.LAPIS_ORE,2, 5),
-        NETHER_QUARTZ_ORE(Material.NETHER_QUARTZ_ORE,2, 5),
-        DIAMOND_ORE(Material.DIAMOND_ORE,3, 7),
-        EMERALD_ORE(Material.EMERALD_ORE,3, 7);
+
+        COAL_ORE(Material.COAL_ORE, 0, 2),
+        DEEPSLATE_COAL_ORE(Material.DEEPSLATE_COAL_ORE, 0, 4),
+        COAL_BLOCK(Material.COAL_BLOCK, 0, 6),
+
+        REDSTONE_ORE(Material.REDSTONE_ORE, 1, 5),
+        DEEPSLATE_REDSTONE_ORE(Material.DEEPSLATE_REDSTONE_ORE, 2, 10),
+        REDSTONE_BLOCK(Material.REDSTONE_BLOCK, 3, 15),
+
+        LAPIS_ORE(Material.LAPIS_ORE, 2, 5),
+        DEEPSLATE_LAPIS_ORE(Material.DEEPSLATE_LAPIS_ORE, 4, 10),
+        LAPIS_BLOCK(Material.LAPIS_BLOCK, 6, 15),
+
+        NETHER_QUARTZ_ORE(Material.NETHER_QUARTZ_ORE, 2, 5),
+        QUARTZ_BLOCK(Material.QUARTZ_BLOCK, 4, 10),
+
+        DIAMOND_ORE(Material.DIAMOND_ORE, 3, 7),
+        DEEPSLATE_DIAMOND_ORE(Material.DEEPSLATE_DIAMOND_ORE, 6, 14),
+        DIAMOND_BLOCK(Material.DIAMOND_BLOCK, 9, 21),
+
+        EMERALD_ORE(Material.EMERALD_ORE, 3, 7),
+        DEEPSLATE_EMERALD_ORE(Material.DEEPSLATE_EMERALD_ORE, 6, 14),
+        EMERALD_BLOCK(Material.EMERALD_BLOCK, 9, 21),
+        ;
 
         private final Material material;
         private final int minExp;
