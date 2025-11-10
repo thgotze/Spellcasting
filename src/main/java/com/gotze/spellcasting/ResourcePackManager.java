97a5cb8140fb93ba85373fdda3be2ab3d7dbc701
Gotze <thorholby@gmail.com>
2025-11-10 10:00:30 +0100

    Refactor ResourcePackManager to use Adventure resource pack api instead of deprecated Bukkit setResourcePack() method

--- src/main/java/com/gotze/spellcasting/ResourcePackManager.java
+++ src/main/java/com/gotze/spellcasting/ResourcePackManager.java
@@ -1,18 +1,36 @@
 package com.gotze.spellcasting;
 
+import net.kyori.adventure.resource.ResourcePackInfo;
+import net.kyori.adventure.resource.ResourcePackRequest;
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.net.URI;
+import java.util.UUID;
+
 public class ResourcePackManager implements Listener {
+
+    private static final URI PACK_URI = URI.create("https://download.mc-packs.net/pack/1596d8f15753ebb16f6870979ad427df8deb880a.zip");
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        // Set resource pack TODO: currently using clientside resourcepack instead of serverside
         Player player = event.getPlayer();
-        String resourcePackUrl = "https://download.mc-packs.net/pack/1596d8f15753ebb16f6870979ad427df8deb880a.zip";
-        player.sendMessage("Setting resource pack from file: " + resourcePackUrl);
-        player.setResourcePack(resourcePackUrl);
+
+        ResourcePackInfo.resourcePackInfo()
+                .uri(PACK_URI)
+                .id(UUID.randomUUID())
+                .computeHashAndBuild()
+                .thenAccept(packInfo -> {
+                    ResourcePackRequest packRequest = ResourcePackRequest.resourcePackRequest()
+                            .packs(packInfo)
+                            .required(true)
+                            .prompt(Component.text("This server uses a custom resource pack."))
+                            .build();
+
+                    player.sendResourcePacks(packRequest);
+                });
     }
 }
