d47e71e055907f309850965a45843c218c1cefad
Gotze <thorholby@gmail.com>
2025-11-16 22:16:30 +0100

    Refactor `MachineManager`: generalize machine handling, remove `Crusher`-specific logic, and streamline event handling.

--- src/main/java/com/gotze/spellcasting/machine/MachineManager.java
+++ src/main/java/com/gotze/spellcasting/machine/MachineManager.java
@@ -1,25 +1,21 @@
 package com.gotze.spellcasting.machine;
 
 import com.gotze.spellcasting.Spellcasting;
-import com.gotze.spellcasting.machine.crusher.Crusher;
 import com.gotze.spellcasting.util.LifecycleManager;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryAction;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitTask;
 
-import java.util.EnumSet;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -54,144 +50,131 @@ public class MachineManager implements Listener, LifecycleManager {
         machines.remove(location);
     }
 
-    @EventHandler
-    public void DEBUGConvertStoneCutterToCrusher(BlockBreakEvent event) {
-        if (event.getBlock().getType() != Material.STONECUTTER) return;
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onPlaceMachine(BlockPlaceEvent event) {
+        ItemStack itemInHand = event.getItemInHand();
+
+        NamespacedKey machineKey = new NamespacedKey(Spellcasting.getPlugin(), "machine");
+        String machineValue = itemInHand
+                .getPersistentDataContainer()
+                .get(machineKey, PersistentDataType.STRING);
+        if (machineValue == null) return;
+
+        Machine.MachineType machineType = Machine.MachineType.valueOf(machineValue.toUpperCase());
 
         Player player = event.getPlayer();
-        if (!player.isSneaking()) return;
+        Location blockLocation = event.getBlockPlaced().getLocation();
 
-        Location blockLocation = event.getBlock().getLocation();
-        if (getMachine(blockLocation) != null) return;
+        Machine newMachine;
+        try {
+            newMachine = machineType.getMachineClass()
+                    .getConstructor(Location.class, Player.class)
+                    .newInstance(blockLocation, player);
+        } catch (InstantiationException | NoSuchMethodException | InvocationTargetException |
+                 IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
 
-        Crusher crusher = new Crusher(blockLocation, player);
-        addMachine(crusher);
-        event.setCancelled(true);
-
+        addMachine(newMachine);
         player.spawnParticle(Particle.HAPPY_VILLAGER, blockLocation.toCenterLocation(), 10, 0.5, 0.5, 0.5);
     }
 
-    @EventHandler
-    public void onPlaceCrusher(BlockPlaceEvent event) {
-        ItemStack itemInHand = event.getItemInHand();
-        if (itemInHand.getType() != Material.STONECUTTER) return;
-
-        NamespacedKey crusherKey = new NamespacedKey(Spellcasting.getPlugin(), "machine");
-        String crusherValue = itemInHand.getPersistentDataContainer().get(crusherKey, PersistentDataType.STRING);
-        if (crusherValue != null) {
-            Player player = event.getPlayer();
-            Location blockLocation = event.getBlockPlaced().getLocation();
-            Crusher crusher = new Crusher(blockLocation, player);
-            addMachine(crusher);
-            player.spawnParticle(Particle.HAPPY_VILLAGER, blockLocation.toCenterLocation(), 10, 0.5, 0.5, 0.5);
-        }
-    }
-
-    @EventHandler
-    public void onBreakCrusher(BlockBreakEvent event) {
-        Location blockLocation = event.getBlock().getLocation();
-        Crusher crusher = (Crusher) getMachine(blockLocation);
-        if (crusher == null) return;
-
-        event.setDropItems(false);
-        World world = blockLocation.getWorld();
-        Location centerCrusherLocation = blockLocation.toCenterLocation();
-
-        world.dropItemNaturally(centerCrusherLocation, crusher.toItemStack());
-
-        ItemStack inputItem = crusher.getInputItem();
-
-        if (inputItem != null) {
-            world.dropItemNaturally(centerCrusherLocation, inputItem);
-        }
-
-        ItemStack outputItem = crusher.getOutputItem();
-        if (outputItem != null) {
-            world.dropItemNaturally(centerCrusherLocation, outputItem);
-        }
-
-        removeMachine(blockLocation);
-    }
-
-    @EventHandler
-    public void onCrusherInWorldRightClick(PlayerInteractEvent event) {
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onMachineRightClick(PlayerInteractEvent event) {
         if (!event.getAction().isRightClick()) return;
 
         Block block = event.getClickedBlock();
         if (block == null) return;
 
-        Crusher crusher = (Crusher) getMachine(block.getLocation());
-        if (crusher == null) return;
+        Machine machine = getMachine(block.getLocation());
+        if (machine == null) return;
 
         event.setCancelled(true);
-        event.getPlayer().openInventory(crusher.getInventory());
+        machine.open(event.getPlayer());
     }
 
-    private static final EnumSet<InventoryAction> ALLOWED_OUTPUT_ACTIONS = EnumSet.of(
-            InventoryAction.PICKUP_ALL,
-            InventoryAction.PICKUP_ONE,
-            InventoryAction.PICKUP_SOME,
-            InventoryAction.PICKUP_HALF,
-            InventoryAction.MOVE_TO_OTHER_INVENTORY,
-            InventoryAction.DROP_ALL_SLOT,
-            InventoryAction.DROP_ONE_SLOT,
-            InventoryAction.PICKUP_ALL_INTO_BUNDLE,
-            InventoryAction.PICKUP_SOME_INTO_BUNDLE
-    );
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onBreakMachine(BlockBreakEvent event) {
+        Location blockLocation = event.getBlock().getLocation();
+        Machine machine = getMachine(blockLocation);
+        if (machine == null) return;
 
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        if (!(event.getInventory().getHolder() instanceof Crusher crusher)) return;
+        event.setDropItems(false);
+        World world = blockLocation.getWorld();
+        Location centerLocation = blockLocation.toCenterLocation();
 
-        // ---------------
-        // Crusher Inventory
-        // ---------------
-        if (event.getClickedInventory() == event.getView().getTopInventory()) {
-            int slot = event.getSlot();
+        // Drop the machine item itself
+        Machine.MachineType type = machine.getMachineType();
+        world.dropItemNaturally(centerLocation, type.getMachineItem());
 
-            // Allow interaction with the input slot
-            if (slot == 11) return;
-
-            // Allow interaction with the output slot if the action is allowed
-            if (slot == 15 && ALLOWED_OUTPUT_ACTIONS.contains(event.getAction())) return;
-
-            // Cancel event for all other slots and output slot if the action wasn't allowed
-            event.setCancelled(true);
-            return;
+        // Drop input/output items
+        ItemStack inputItem = machine.getInputItem();
+        ItemStack outputItem = machine.getOutputItem();
+        if (inputItem != null) {
+            world.dropItemNaturally(centerLocation, inputItem);
+        }
+        if (outputItem != null) {
+            world.dropItemNaturally(centerLocation, outputItem);
         }
 
-        // ---------------
-        // Player Inventory
-        // ---------------
-        ClickType clickType = event.getClick();
-        if (clickType != ClickType.SHIFT_LEFT && clickType != ClickType.SHIFT_RIGHT) return;
-
-        ItemStack clickedItem = event.getCurrentItem();
-        if (clickedItem == null) return;
-
-        ItemStack inputItem = crusher.getInputItem();
-        if (inputItem == null) return;
-
-        if (clickedItem.isSimilar(inputItem)) {
-            int clickedItemAmount = clickedItem.getAmount();
-            int inputItemAmount = inputItem.getAmount();
-
-            if (clickedItemAmount + inputItemAmount <= 64) return;
-
-            int transferred = 64 - inputItemAmount;
-            inputItem.setAmount(64);
-            clickedItem.subtract(transferred);
-        }
-        event.setCancelled(true);
+        removeMachine(blockLocation);
     }
 
-
-    @EventHandler
-    public void onInventoryDrag(InventoryDragEvent event) {
-        if (!(event.getInventory().getHolder() instanceof Crusher)) return;
-        // Cancel event if player tries to drag an item to the output slot (15)
-        if (event.getRawSlots().contains(15)) {
-            event.setCancelled(true);
-        }
-    }
+//    private static final EnumSet<InventoryAction> ALLOWED_OUTPUT_ACTIONS = EnumSet.of(
+//            InventoryAction.PICKUP_ALL,
+//            InventoryAction.PICKUP_ONE,
+//            InventoryAction.PICKUP_SOME,
+//            InventoryAction.PICKUP_HALF,
+//            InventoryAction.MOVE_TO_OTHER_INVENTORY,
+//            InventoryAction.DROP_ALL_SLOT,
+//            InventoryAction.DROP_ONE_SLOT,
+//            InventoryAction.PICKUP_ALL_INTO_BUNDLE,
+//            InventoryAction.PICKUP_SOME_INTO_BUNDLE
+//    );
+//
+//    @EventHandler
+//    public void onInventoryClick(InventoryClickEvent event) {
+//        if (!(event.getInventory().getHolder() instanceof Crusher crusher)) return;
+//
+//        // ---------------
+//        // Crusher Inventory
+//        // ---------------
+//        if (event.getClickedInventory() == event.getView().getTopInventory()) {
+//            int slot = event.getSlot();
+//
+//            // Allow interaction with the input slot
+//            if (slot == 11) return;
+//
+//            // Allow interaction with the output slot if the action is allowed
+//            if (slot == 15 && ALLOWED_OUTPUT_ACTIONS.contains(event.getAction())) return;
+//
+//            // Cancel event for all other slots and output slot if the action wasn't allowed
+//            event.setCancelled(true);
+//            return;
+//        }
+//
+//        // ---------------
+//        // Player Inventory
+//        // ---------------
+//        ClickType clickType = event.getClick();
+//        if (clickType != ClickType.SHIFT_LEFT && clickType != ClickType.SHIFT_RIGHT) return;
+//
+//        ItemStack clickedItem = event.getCurrentItem();
+//        if (clickedItem == null) return;
+//
+//        ItemStack inputItem = crusher.getInputItem();
+//        if (inputItem == null) return;
+//
+//        if (clickedItem.isSimilar(inputItem)) {
+//            int clickedItemAmount = clickedItem.getAmount();
+//            int inputItemAmount = inputItem.getAmount();
+//
+//            if (clickedItemAmount + inputItemAmount <= 64) return;
+//
+//            int transferred = 64 - inputItemAmount;
+//            inputItem.setAmount(64);
+//            clickedItem.subtract(transferred);
+//        }
+//        event.setCancelled(true);
+//    }
 }
