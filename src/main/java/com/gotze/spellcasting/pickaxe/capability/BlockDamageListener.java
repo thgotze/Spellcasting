41eb51401894b134dfaaba131ec346d53772e286
Gotze <thorholby@gmail.com>
2025-11-17 22:00:24 +0100

    Clarify parameter descriptions in `BlockDamageListener` documentation for improved readability.

--- src/main/java/com/gotze/spellcasting/pickaxe/capability/BlockDamageListener.java
+++ src/main/java/com/gotze/spellcasting/pickaxe/capability/BlockDamageListener.java
@@ -8,9 +8,9 @@ import org.bukkit.event.block.BlockDamageEvent;
 public interface BlockDamageListener {
     /**
      * Called when a player damages a block
-     * @param player The player breaking the block
+     * @param player The player damaging the block
      * @param event The BlockDamageEvent
-     * @param pickaxeData The pickaxe data
+     * @param pickaxeData The pickaxe data associated with the player
      */
     void onBlockDamage(Player player, BlockDamageEvent event, PickaxeData pickaxeData);
 }
