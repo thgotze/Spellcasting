1e8953e940ee6421c753d4bba129b2a8b662c520
Gotze <thorholby@gmail.com>
2025-11-24 23:52:43 +0100

    Add SpawnCommand

---
+++ src/main/java/com/gotze/spellcasting/command/SpawnCommand.java
@@ -0,0 +1,18 @@
+package com.gotze.spellcasting.command;
+
+import com.gotze.spellcasting.Spellcasting;
+import io.papermc.paper.command.brigadier.BasicCommand;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class SpawnCommand implements BasicCommand {
+
+    @Override
+    public void execute(@NotNull CommandSourceStack commandSourceStack, String @NotNull [] args) {
+        if (!(commandSourceStack.getSender() instanceof Player player)) return;
+        player.teleport(new Location(player.getWorld(), 0.5, 100, 0.5));
+    }
+}
