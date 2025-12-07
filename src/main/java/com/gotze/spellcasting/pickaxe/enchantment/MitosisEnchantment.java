99195695fcbc82fcf6e5a0c8fed64aa401212b7c
Gotze <thorholby@gmail.com>
2025-12-08 00:43:04 +0100

    Revamp MitosisEnchantment with activation chance, sound feedback, particle visual fedeback

--- src/main/java/com/gotze/spellcasting/pickaxe/enchantment/MitosisEnchantment.java
+++ src/main/java/com/gotze/spellcasting/pickaxe/enchantment/MitosisEnchantment.java
@@ -5,6 +5,10 @@ import com.gotze.spellcasting.pickaxe.capability.BlockBreakListener;
 import com.gotze.spellcasting.pickaxe.capability.BlockDamageListener;
 import com.gotze.spellcasting.util.block.BlockCategories;
 import com.gotze.spellcasting.util.block.BlockUtils;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
@@ -30,8 +34,7 @@ public class MitosisEnchantment extends Enchantment implements BlockBreakListene
     public void onBlockBreak(Player player, Block block, PickaxeData pickaxeData, boolean isNaturalBreak) {
         if (!isNaturalBreak) return;
         if (!BlockCategories.ORE_BLOCKS.containsKey(block.getType())) return;
-        // 5% activation chance
-//        if (ThreadLocalRandom.current().nextDouble() > 0.05) return;
+        if (ThreadLocalRandom.current().nextDouble() > (0.02 + getLevel() * 0.02)) return; // 4%, 6%, 8%, 10%, 12%
 
         List<Block> candidateBlocks = switch (blockFace) {
             case NORTH, SOUTH -> BlockUtils.getBlocksInSquarePattern(block, 3, 3, 1);
@@ -45,10 +48,20 @@ public class MitosisEnchantment extends Enchantment implements BlockBreakListene
 
         Block chosenBlock1 = candidateBlocks.remove(ThreadLocalRandom.current().nextInt(candidateBlocks.size()));
         chosenBlock1.setType(block.getType());
-
+        player.playSound(chosenBlock1.getLocation(), Sound.ITEM_BONE_MEAL_USE, 1f, 1f);
+        player.playEffect(chosenBlock1.getLocation(), Effect.STEP_SOUND, block.getType());
+        for (Location blockOutlineForParticle : BlockUtils.getBlockOutlineForParticles(chosenBlock1.getLocation(), 0.10)) {
+//            player.spawnParticle(Particle.BLOCK, blockOutlineForParticle, 0, 0, 0, 0, 0, block.getBlockData());
+            player.spawnParticle(Particle.TOTEM_OF_UNDYING, blockOutlineForParticle, 0, 0, 0, 0, 0);
+        }
         if (candidateBlocks.isEmpty()) return;
 
         Block chosenBlock2 = candidateBlocks.remove(ThreadLocalRandom.current().nextInt(candidateBlocks.size()));
         chosenBlock2.setType(block.getType());
+        player.playSound(chosenBlock2.getLocation(), Sound.ITEM_BONE_MEAL_USE, 1f, 1f);
+        player.playEffect(chosenBlock2.getLocation(), Effect.STEP_SOUND, block.getType());
+        for (Location blockOutlineForParticle : BlockUtils.getBlockOutlineForParticles(chosenBlock2.getLocation(), 0.10)) {
+            player.spawnParticle(Particle.TOTEM_OF_UNDYING, blockOutlineForParticle, 0, 0, 0, 0, 0);
+        }
     }
 }
