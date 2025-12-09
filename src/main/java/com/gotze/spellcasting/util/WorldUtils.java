11f1cb28fc519ad810bd80050b4d07c16e81bcec
Gotze <thorholby@gmail.com>
2025-12-09 17:07:46 +0100

    Mark `getWorld` method in `WorldUtils` as `@NotNull` and enforce null safety with `Objects.requireNonNull`

--- src/main/java/com/gotze/spellcasting/util/WorldUtils.java
+++ src/main/java/com/gotze/spellcasting/util/WorldUtils.java
@@ -2,12 +2,15 @@ package com.gotze.spellcasting.util;
 
 import com.gotze.spellcasting.Spellcasting;
 import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class WorldUtils {
 
     private static final World world = Spellcasting.getPlugin().getServer().getWorld("world");
 
-    public static World getWorld() {
-        return world;
+    public static @NotNull World getWorld() {
+        return Objects.requireNonNull(world);
     }
 }
